c0037b511470d611b2d497903fef6eca0978f338
syd40 <128923029+syd40@users.noreply.github.com>
2023-04-18 08:04:11 +0530

    Added Cart Functionality

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -21,7 +23,9 @@ export class ProductListComponent implements OnInit {
   theTotalElements: number = 0;
   previousKeyword: string = "";
 
-  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+  constructor(private productService: ProductService,
+    private cartService: CartService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
 
@@ -94,6 +98,12 @@ export class ProductListComponent implements OnInit {
       this.thePageSize = data.page.size;
       this.theTotalElements = data.page.totalElements;
     }
+  }
+
+  addToCart(theProduct: Product){
+    console.log(`Adding To Cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+    const theCartItem = new CartItem(theProduct);
+    this.cartService.addToCart(theCartItem);
   }
 
 }
