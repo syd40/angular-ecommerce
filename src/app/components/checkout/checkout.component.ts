08f0a420a122044bc92ceae96410ee105325f3e8
Shubham Yadav <128923029+syd40@users.noreply.github.com>
2023-05-09 18:53:13 +0530

    Added price and quantity to order review

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
+import { CartService } from 'src/app/services/cart.service';
 import { FormService } from 'src/app/services/form.service';
 import { CustomValidators } from 'src/app/validators/custom-validators';
 
@@ -26,9 +27,14 @@ export class CheckoutComponent implements OnInit {
   shippingAddressStates: State[] = [];
   billingAddressStates: State[] = [];
   
-  constructor(private formBuilder: FormBuilder, private formService: FormService) { }
+  constructor(private formBuilder: FormBuilder, private formService: FormService, private cartService: CartService) { }
 
   ngOnInit(): void {
+
+    this.reviewCardDetails();
+
+
+
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: new FormControl('',[Validators.required, Validators.minLength(2), CustomValidators.notOnlyWhiteSpace]),
@@ -79,6 +85,17 @@ export class CheckoutComponent implements OnInit {
       }
     );
 
+  }
+  reviewCardDetails() {
+    
+    this.cartService.totalQuantity.subscribe(
+      (data) => { this.totalQuantity = data }
+    );
+
+    this.cartService.totalPrice.subscribe(
+      (data) => { this.totalPrice = data }
+    );
+    
   }
 
   get firstName(){ return this.checkoutFormGroup.get('customer.firstName');}
